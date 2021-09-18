77ec09ed5fd4b87561878864c5d2de43225173d4
olegStacevich <oleg295832611@gmail.com>
2021-09-18 22:14:42 +0200

    refactor

--- src/components/nav/NavMenuBig.tsx
+++ src/components/nav/NavMenuBig.tsx
@@ -24,61 +24,7 @@ console.log(props.item)
                            color: "#FB2E86"
                         }}>{item.title}</NavLink>
                     </div>
-
-
                 })}
-
-            {/*<div>*/}
-            {/*<img className={style.photo} src={photo} alt="photo"/>*/}
-            {/*<NavLink to={PATH. PRODUCT} className={style.link} activeStyle={{*/}
-            {/*    color: "#FB2E86"*/}
-            {/*}}>Сервизы столовые</NavLink>*/}
-            {/*</div>*/}
-            {/*<NavLink to={PATH. PRODUCT1} className={style.link} activeStyle={{*/}
-            {/*    color: "#FB2E86"*/}
-            {/*}}>Сервизы чайные</NavLink>*/}
-            {/*<NavLink to={PATH. PRODUCT2} className={style.link} activeStyle={{*/}
-            {/*    color: "#FB2E86"*/}
-            {/*}}>Тарелки</NavLink>*/}
-            {/*<NavLink to={PATH. PRODUCT3} className={style.link} activeStyle={{*/}
-            {/*    color: "#FB2E86"*/}
-            {/*}}>Салатники</NavLink>*/}
-            {/*<NavLink to={PATH. PRODUCT4} className={style.link} activeStyle={{*/}
-            {/*    color: "#FB2E86"*/}
-            {/*}}>Блюда/Селедочницы</NavLink>*/}
-            {/*<NavLink to={PATH. PRODUCT5} className={style.link} activeStyle={{*/}
-            {/*    color: "#FB2E86"*/}
-            {/*}}></NavLink>*/}
-            {/*<NavLink to={PATH. PRODUCT6} className={style.link} activeStyle={{*/}
-            {/*    color: "#FB2E86"*/}
-            {/*}}>Сервизы чайные</NavLink>*/}
-            {/*<NavLink to={PATH. PRODUCT7} className={style.link} activeStyle={{*/}
-            {/*    color: "#FB2E86"*/}
-            {/*}}>Сервизы чайные</NavLink>*/}
-            {/*<NavLink to={PATH. PRODUCT8} className={style.link} activeStyle={{*/}
-            {/*    color: "#FB2E86"*/}
-            {/*}}>Сервизы чайные</NavLink>*/}
-            {/*<NavLink to={PATH. PRODUCT9} className={style.link} activeStyle={{*/}
-            {/*    color: "#FB2E86"*/}
-            {/*}}>Сервизы чайные</NavLink>*/}
-            {/*<NavLink to={PATH. PRODUCT10} className={style.link} activeStyle={{*/}
-            {/*    color: "#FB2E86"*/}
-            {/*}}>Сервизы чайные</NavLink>*/}
-            {/*<NavLink to={PATH. PRODUCT11} className={style.link} activeStyle={{*/}
-            {/*    color: "#FB2E86"*/}
-            {/*}}>Сервизы чайные</NavLink>*/}
-            {/*<NavLink to={PATH. PRODUCT12} className={style.link} activeStyle={{*/}
-            {/*    color: "#FB2E86"*/}
-            {/*}}>Сервизы чайные</NavLink>*/}
-            {/*<NavLink to={PATH. PRODUCT13} className={style.link} activeStyle={{*/}
-            {/*    color: "#FB2E86"*/}
-            {/*}}>Сервизы чайные</NavLink>*/}
-            {/*<NavLink to={PATH. PRODUCT14} className={style.link} activeStyle={{*/}
-            {/*    color: "#FB2E86"*/}
-            {/*}}>Сервизы чайные</NavLink>*/}
-            {/*<NavLink to={PATH. PRODUCT15} className={style.link} activeStyle={{*/}
-            {/*    color: "#FB2E86"*/}
-            {/*}}>Сервизы чайные</NavLink>*/}
         </div>
     );
 };
